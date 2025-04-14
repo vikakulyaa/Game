63f75f2b60f865e35c0c48279a83fd4629de1a40
vikakulyaa <viktoriaalekxeeva@gmail.com>
2025-04-14 22:17:31 +0300

    Add files via upload

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(20, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(67, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Jusu cipars: 1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(136, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 70);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(252, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 70);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Pakāpju skaits: 15";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(393, 201);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ceļš uz 100";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1: Form
+    {
+        static Random rnd = new Random();
+        int countRes = 15;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateBut();
+        }
+
+        private void UpdateBut() 
+        {
+            int rndint;
+            Button[] but = { button1, button2, button3 };
+            for (int i = 0; i < but.Length; i++)
+            {
+                    rndint = GetRandomInt();
+                    but[i].Text = GetRandomMathSymbol(but[i], rndint).ToString() + rndint;
+            }
+        }
+
+        private char GetRandomMathSymbol(Button btn, int rndint)
+        {
+            int currentNumber = Int32.Parse(Regex.Replace(label1.Text, @"\D", ""));
+            char[] allSymbols = { '+', '*', '-', '/' };
+            char[] firstStepSymbols = { '+', '*' };
+            char[] smallDrop = { '*', '/' };
+            char[] nextDrop = { '+', '-' };
+
+
+
+            if (currentNumber >= 71)
+            {
+                return nextDrop[rnd.Next(nextDrop.Length)];
+            }
+            else if (currentNumber >= 20 && currentNumber <= 70)
+            {
+                if (RandomInt() <= 30 && rndint <= 3)
+                {
+                    return smallDrop[rnd.Next(smallDrop.Length)];
+                } else {
+                    if (RandomInt() <= 40 && rndint <= 3)
+                    {
+                       return firstStepSymbols[rnd.Next(firstStepSymbols.Length)];
+                    }
+                    else
+                    {
+                        return nextDrop[rnd.Next(nextDrop.Length)];
+                    }
+                }
+            }
+            else 
+            {
+                if (rndint <= 3)
+                {
+                    return firstStepSymbols[rnd.Next(firstStepSymbols.Length)];
+                }
+                else
+                {
+                    return '+';
+                }
+            }
+        }
+
+
+        static int RandomInt()
+        {
+            return rnd.Next(1,100);
+        }
+        private int GetRandomInt()
+        {
+            int currentNumber = Int32.Parse(Regex.Replace(label1.Text, @"\D", ""));
+
+            int[] startlvl = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3 };
+            int[] midlvl = { 2, 3, 5, 7, 9, 10, 2};
+            int[] hight = { 1, 2, 3, 4, 5 };
+
+            if (currentNumber >= 90) 
+                return hight[rnd.Next(hight.Length)];
+            else if (currentNumber >= 20)
+                return midlvl[rnd.Next(midlvl.Length)];
+            else
+                return startlvl[rnd.Next(startlvl.Length)];
+            
+        
+            }
+
+        private void click(Button btn) 
+        {
+            if (countRes != 0)
+            {
+                int currentNumber = Int32.Parse(Regex.Replace(label1.Text, @"\D", ""));
+                int operand = Int32.Parse(Regex.Replace(btn.Text, @"\D", ""));
+                char operation = btn.Text.First(c => "+-*/".Contains(c));
+
+                switch (operation)
+                {
+                    case '+':
+                        currentNumber += operand;
+                        countRes -= 1;
+                        break;
+                    case '-':
+                        currentNumber -= operand;
+                        countRes -= 1;
+                        break;
+                    case '*':
+                        currentNumber *= operand;
+                        countRes -= 1;
+                        break;
+                    case '/':
+                        if (operand != 0)
+                            currentNumber /= operand;
+                        countRes -= 1;
+                        break;
+                }
+
+                label1.Text = "Jusu cipars: " + currentNumber;
+                label2.Text = "Pakāpju skaits: " + (countRes).ToString();
+
+                if (currentNumber == 100)
+                {
+                    MessageBox.Show("Apsveicam! Jūs sasniedzāt 100!", "Uzvara", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ResetGame();
+                }
+                else if (currentNumber > 100 || currentNumber <= 0 || countRes <= 0)
+                {
+                    MessageBox.Show("Spēle beigusies! Mēģiniet vēlreiz.", "Beigas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ResetGame();
+                }
+                else
+                {
+                    UpdateBut();
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Spēle beigusies! Mēģiniet vēlreiz.", "Beigas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ResetGame();
+
+            }
+        }
+        private void ResetGame()
+        {
+            countRes = 15;
+            label1.Text = "Jusu cipars: 1";
+            label2.Text = "Pakāpju skaits: " + countRes;
+            UpdateBut();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            click(button1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            click(button2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            click(button3);
+        }
+    }
+}
